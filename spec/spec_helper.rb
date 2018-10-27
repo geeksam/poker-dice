6010465b0a782dd52510452d913055272a4acd2e
Sam Livingston-Gray <geeksam@gmail.com>
2018-10-27 15:39:44 -0700

    Allow focusing specs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -22,5 +22,12 @@ RSpec.configure do |config|
 
   config.order = :defined # keep things nice and predictable for folks just getting started
 
+  # These two settings work together to allow you to limit a spec run to
+  # individual examples or groups you care about by tagging them with
+  # `:focus` metadata. When nothing is tagged with `:focus`, all
+  # examples get run.
+  config.filter_run :focus
+  config.run_all_when_everything_filtered = true
+
   config.include SpecHelpers
 end
